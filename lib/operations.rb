ac0642bb84d6f5a33ffa0dad0b53a79a1b96cfee
Ashley  Zaki <ahnahn.un@gmail.com>
2018-10-10 02:21:07 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -3,7 +3,7 @@ def unsafe?(speed)
   if speed < 40 && speed > 60
     return "Going #{speed} mph on the freeway would be unsafe"
   else
-    return "false"
+    return "Going #{speed} miles per hour"
 end
 
 
